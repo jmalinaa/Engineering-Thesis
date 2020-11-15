34b6d880124d5f1abc6955ce724b9b613b6253af
Karol <karolbielen@gmail.com>
2020-11-15 18:12:30 +0100

    Refactor REST GET method - now it's extracted to separate file

--- Frontend/src/pages/util/REST/GET.js
+++ Frontend/src/pages/util/REST/GET.js
@@ -1,20 +1,61 @@
-import { BASE_PATH } from "./paths";
+export default function GET(path, onSuccess, onError) {
 
-export default async function GET(relativePath, onSuccess, onError) {
-
+    let errorOccurred = false;
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
     const init = {
         method: 'GET',
         headers: headers,
-        cache: 'default'
+        cache: 'default',
+        // mode: 'no-cors'
     };
 
-    // let response = await fetch(BASE_PATH + relativePath, init)
-    // let json = await response.json;
-    // return json;
-    fetch(BASE_PATH + relativePath)
-        .then((response) => onSuccess(response))
-        .then(data => console.log("GET, data:", data))
+    function log(promise) {
+        console.log("GET, path: ", path);
+        console.log("GET, promise: ", promise);
+        return promise;
+    }
+
+    function resolve(promise) {
+        console.log("GET, resolve, promise: ", promise);
+        return Promise.resolve(promise);
+    }
+
+    function checkStatus(response) {
+        if (!response.ok) {
+            errorOccurred = true;
+            onError('Wystąpił błąd. Status odpowiedzi: ' + response.status);
+            return null;
+        }
+        return response;
+    }
+
+    function getJson(response) {
+        console.log("GET, getJson, response: ", response);
+        if (response == null)
+            return null;
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('application/json')) {
+            errorOccurred = true;
+            throw new TypeError("Oops, we haven't got JSON!");  //TODO test this!
+        }
+        return response.json();
+    }
+
+    function onSuccessWrapper(json) {
+        if(!errorOccurred)
+            onSuccess(json)
+    }
+
+    fetch(path)
+        .then(log)
+        .then(resolve.catch)
+        .then(checkStatus)
+        .then(getJson)
+        .then(onSuccessWrapper)
+        .catch(onError)
+    //Następujący błąd występujący w Firefoxie:
+    //Błąd mapy źródła: Error: NetworkError when attempting to fetch resource.
+    //należy zignorować, wynika z buga w tej przeglądarce
 }
